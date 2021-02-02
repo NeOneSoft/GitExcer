0c88b057ff65cb3d3bc535b3780cb15302e7cd89
Gonzalo <gonzalo.romero@edunext.co>
2021-02-02 14:03:43 -0600

    ADD class Car

--- classes.py
+++ classes.py
@@ -4,4 +4,18 @@ Define Class Person
 
 
 class Person:
-    def __init__(self, first_name, last_name)
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+"""
+Define Class Car
+"""
+
+
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
