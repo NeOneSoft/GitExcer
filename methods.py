da8fac4b27a5adf93337ae5dd073481349050992
Gonzalo <gonzalo.romero@edunext.co>
2021-02-02 15:05:08 -0600

    ADD method Age

---
+++ methods.py
@@ -0,0 +1,22 @@
+"""
+Name
+"""
+
+
+def name(name):
+    print("My name is {}".format(name))
+
+
+name("Gonzalo")
+
+
+"""
+Age
+"""
+
+
+def age(age):
+    print("My age is {}".format(age))
+
+
+name(36)
